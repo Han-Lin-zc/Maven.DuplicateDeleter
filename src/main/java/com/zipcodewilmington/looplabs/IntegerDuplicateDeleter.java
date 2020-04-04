d5db6423372ed845ef80375d359600e972073b87
Han-Lin-zc <hlin04@gmail.com>
2020-04-04 17:48:07 -0400

    Done Lab

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,61 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    Integer[] arr = Arrays.copyOf(array, array.length);
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] tempArr = new Integer[array.length];
+        int newArrLength = 0;
+        for (Integer i : arr) {
+            if (countOccurrences(i) < maxNumberOfDuplications) {
+                tempArr[newArrLength] = i;
+                newArrLength++;
+            }
+        }
+        Integer[] result = new Integer[newArrLength];
+        for (int i = 0; i < newArrLength; i++) {
+            result[i] = tempArr[i];
+        }
+        return result;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Integer[] tempArr = new Integer[array.length];
+        int newArrLength = 0;
+        for (Integer i : arr) {
+            if (countOccurrences(i) != exactNumberOfDuplications) {
+                tempArr[newArrLength] = i;
+                newArrLength++;
+            }
+        }
+        Integer[] result = new Integer[newArrLength];
+        for (int i = 0; i < newArrLength; i++) {
+            result[i] = tempArr[i];
+        }
+        return result;
+    }
+
+    public Integer countOccurrences(Integer value) {
+        Integer counter = 0;
+        for (Integer i : arr) {
+            if (i.equals(value)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,62 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    String[] test = Arrays.copyOf(array, array.length);
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] tempArr = new String[array.length];
+        int newArrLength = 0;
+        for (String s : array) {
+            int occurrences = countOccurrences(s);
+            if (occurrences < maxNumberOfDuplications) {
+                tempArr[newArrLength] = s;
+                newArrLength++;
+            }
+        }
+        String[] result = new String[newArrLength];
+        for (int i = 0; i < newArrLength; i++) {
+            result[i] = tempArr[i];
+        }
+        return result;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] tempArr = new String[array.length];
+        int newArrLength = 0;
+        for (String s : test) {
+            int occurrences = countOccurrences(s);
+            if (occurrences != exactNumberOfDuplications) {
+                tempArr[newArrLength] = s;
+                newArrLength++;
+            }
+        }
+        String[] result = new String[newArrLength];
+        for (int i = 0; i < newArrLength; i++) {
+            result[i] = tempArr[i];
+        }
+        return result;
+    }
+
+    public Integer countOccurrences(String value) {
+        Integer counter = 0;
+        for (String s : test) {
+            if (s.equals(value)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
